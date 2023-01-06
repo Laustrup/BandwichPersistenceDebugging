bc34044fee20833d033a145a738aceac80ec14c0
Laust <laust.bonnesen@mail.com>
2023-01-06 01:48:03 +0100

    Initial commit

---
+++ src/main/java/laustrup/bandwichpersistencedebugging/BandwichPersistenceDebuggingApplication.java
@@ -0,0 +1,13 @@
+package laustrup.bandwichpersistencedebugging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandwichPersistenceDebuggingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BandwichPersistenceDebuggingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/laustrup/bandwichpersistencedebugging/BandwichPersistenceDebuggingApplicationTests.java
@@ -0,0 +1,13 @@
+package laustrup.bandwichpersistencedebugging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandwichPersistenceDebuggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
